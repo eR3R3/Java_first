238d7524e515219bb78cf357709183f7198e9241
eR3R3 <er1r1@qq.com>
2024-10-11 19:53:47 +0800

    I build a new repository

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle A = new Rectangle();
+        System.out.printf("this is %d and %d", A.height, A.width);
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    public int height;
+    public int width;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle() {
+        this(5, 7);
+    }
+}
